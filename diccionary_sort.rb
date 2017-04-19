46b51cc44528b0a2d39b4e9c4626bc90a3cc54ef
Armando80 <fc.armando@outlook.com>
2017-04-18 22:37:57 -0500

    Con Mayusculas y ordenado

--- diccionary_sort.rb
+++ diccionary_sort.rb
@@ -1,17 +1,18 @@
 words =[]
 
 p "Escribe una palabra:"
-	word = gets.chomp
+	word = gets.chomp.capitalize
 	words << word
 	i = 0
+
 	loop do 
-	p "Escribe otra palabra(o presiona 'enter' para finalizar):"
-		word = gets.chomp
+p "Escribe otra palabra(o presiona 'enter' para finalizar):"
+		word = gets.chomp.capitalize
 		i += 1
 		break if word == ""
 		words << word
 	end	
 puts "Felicidades: Tu diccionario tiene #{i} palabras :"
-puts words
+puts words.sort
 
 
