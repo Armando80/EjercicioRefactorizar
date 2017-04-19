13561a95eed1e58da0b9d24226e5594ba2ea55b4
Armando80 <fc.armando@outlook.com>
2017-04-19 18:28:52 -0500

    No deben Capitalizarse las palabras

--- diccionary_sort.rb
+++ diccionary_sort.rb
@@ -1,18 +1,18 @@
 words =[]
 
 p "Escribe una palabra:"
-	word = gets.chomp.capitalize
+	word = gets.chomp
 	words << word
 	i = 0
 
 	loop do 
 p "Escribe otra palabra(o presiona 'enter' para finalizar):"
-		word = gets.chomp.capitalize
+		word = gets.chomp
 		i += 1
 		break if word == ""
 		words << word
 	end	
 puts "Felicidades: Tu diccionario tiene #{i} palabras :"
-puts words.sort
+puts words
 
 
