85083dd034bcd50bd09c759ea7c7a307cab932e0
Armando80 <fc.armando@outlook.com>
2017-04-18 19:46:18 -0500

    Diccionario sin mayusculas

---
+++ diccionary_sort.rb
@@ -0,0 +1,17 @@
+words =[]
+
+p "Escribe una palabra:"
+	word = gets.chomp
+	words << word
+	i = 0
+	loop do 
+	p "Escribe otra palabra(o presiona 'enter' para finalizar):"
+		word = gets.chomp
+		i += 1
+		break if word == ""
+		words << word
+	end	
+puts "Felicidades: Tu diccionario tiene #{i} palabras :"
+puts words
+
+
